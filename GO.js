62474a5a0356292a9cfa25e5b895f19910802ab9
xdcell <40189887+xdcell@users.noreply.github.com>
2018-06-29 23:29:38 +0700

    Add files via upload

--- GO.js
+++ GO.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.caranyakami.tech/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "http://acakhp.blogspot.com/p/silakan-tekan-tombol-continue-untuk.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
